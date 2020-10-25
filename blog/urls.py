b8176f781058457d31d455745fc32095becbd4ff
zzgzjun <2589641825@qq.com>
2020-10-26 06:45:08 +0800

    完成基本注册、登录、注销、评论功能（部门链接功能未完成）

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('',views.index),
     path('index/',views.index,name='index'),
     path('archive/',views.archive,name='archive'),
+    path('article/',views.article,name='article'),
+
+    path(r'logout/', views.do_logout, name='logout'),
+    path(r'reg/', views.do_reg, name='reg'),
+    path(r'login/', views.do_login, name='login'),
+    path(r'comment/post/', views.comment_post, name='comment_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.hashers import make_password
 from django.db import connection
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from .models import *
 from django.core.paginator import Paginator,InvalidPage,EmptyPage,PageNotAnInteger
 from django.db.models import Count
+from .forms import *
 # Create your views here.
 
 # 全局变量配置-返回函数
@@ -31,9 +34,6 @@ def global_setting(request):
     article_comment_list=[Article.objects.get(pk=comment['article']) for comment in comment_count_list ]
     return locals()
 
-
-
-
 def index(request):
     article_list = getPage(request, Article.objects.all())
     return render(request,'index.html',locals())
@@ -57,3 +57,107 @@ def archive(request):
     article_list=getPage(request,article_list)
     print(year,month)
     return render(request, 'archive.html', locals())
+
+
+def article(request):
+
+    pk = request.GET.get('pk', None)
+    try:
+    # 获取文章信息
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return render(request, 'failure.html', {'reason': '没有找到对应的文章'})
+
+    #  # 评论表单
+    # comment_form = CommentForm({'author': request.user.username,
+    #                             'email': request.user.email,
+    #                             'url': request.user,
+    #                             'article': id} if request.user.is_authenticated() else {'article': pk})
+    # print(request.user)
+    if request.user.is_authenticated:
+        comment_form = CommentForm({'author': request.user.username,
+                                    'email': request.user.email,
+                                    'url': request.user.url,
+                                    'article': pk})
+    else:
+        comment_form=CommentForm({'article': pk})
+
+        # 获取评论信息
+    comments = Comment.objects.filter(article=article).order_by('id')
+    comment_list = []
+    for comment in comments:
+        for item in comment_list:
+            if not hasattr(item, 'children_comment'):
+                setattr(item, 'children_comment', [])
+            if comment.pid == item:
+                item.children_comment.append(comment)
+                break
+        if comment.pid is None:
+            comment_list.append(comment)
+    return render(request, 'article.html', locals())
+
+
+
+# 注销
+def do_logout(request):
+    logout(request)
+    return redirect(request.META['HTTP_REFERER'])
+
+# 注册
+def do_reg(request):
+
+    if request.method == 'POST':
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+                # 注册
+            user = User.objects.create(username=reg_form.cleaned_data["username"],
+                                    email=reg_form.cleaned_data["email"],
+                                    url=reg_form.cleaned_data["url"],
+                                    password=make_password(reg_form.cleaned_data["password"]),)
+            user.save()
+                # 登录
+            user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
+            login(request, user)
+            return redirect(request.POST.get('source_url'))
+        else:
+            return render(request, 'failure.html', {'reason': reg_form.errors})
+    else:
+        reg_form = RegForm()
+    return render(request, 'reg.html', locals())
+
+# 登录
+def do_login(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+                # 登录
+            username = login_form.cleaned_data["username"]
+            password = login_form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
+                login(request, user)
+            else:
+                return render(request, 'failure.html', {'reason': '登录验证失败'})
+            return redirect(request.POST.get('source_url'))
+        else:
+            return render(request, 'failure.html', {'reason': login_form.errors})
+    else:
+        login_form = LoginForm()
+    return render(request, 'login.html', locals())
+
+# 提交评论
+def comment_post(request):
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+         #获取表单信息
+        comment = Comment.objects.create(username=comment_form.cleaned_data["author"],
+                                             email=comment_form.cleaned_data["email"],
+                                             url=comment_form.cleaned_data["url"],
+                                             content=comment_form.cleaned_data["comment"],
+                                             article_id=comment_form.cleaned_data["article"],
+                                             user=request.user if request.user.is_authenticated else None)
+        comment.save()
+    else:
+        return render(request, 'failure.html', {'reason': comment_form.errors})
+    return redirect(request.META['HTTP_REFERER'])
